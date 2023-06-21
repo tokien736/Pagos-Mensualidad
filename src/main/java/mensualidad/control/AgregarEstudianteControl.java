b0bc3e5382414483d1ce90260f95043d96bddf24
tokien736 <72504407@continental.edu.pe>
2023-06-21 10:47:40 -0500

    Primera parte -Creamos la estructura de carpetas del proyecto -Creamos las ventanas de navegación y controladores de ventana. -También creamos una clase llamada "Estudiante". -NOTA: Nuestro repositorio anterior se corrompió o tenemos problemas con las ramas. esta bien escrito

---
+++ src/main/java/mensualidad/control/AgregarEstudianteControl.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mensualidad.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author USUARIO
+ */
+public class AgregarEstudianteControl implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        primaryStage.close();
+    }
+    
+}
